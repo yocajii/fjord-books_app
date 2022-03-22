636bf0215327171212571716d1d78761a7e3c3e4
yocajii <yocajii@stone.sakura.ne.jp>
2022-03-22 11:01:33 +0900

    i18nに対応した
    - 辞書ファイル作成
    - view書き換え
    - クエリパラメーターによるlocale切り替え機能を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  private
+
+  def default_url_options(options = {})
+    { locale: I18n.locale }.merge options
+  end
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 class BooksController < ApplicationController
+  before_action :set_locale
   before_action :set_book, only: %i[show edit update destroy]
 
   # GET /books
   # GET /books.json
   def index
     @books = Book.all
+    @locale = I18n.locale
   end
 
   # GET /books/1
@@ -28,7 +30,7 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       if @book.save
-        format.html { redirect_to @book, notice: 'Book was successfully created.' }
+        format.html { redirect_to @book, notice: t('messages.success.create') }
         format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new }
@@ -42,7 +44,7 @@ class BooksController < ApplicationController
   def update
     respond_to do |format|
       if @book.update(book_params)
-        format.html { redirect_to @book, notice: 'Book was successfully updated.' }
+        format.html { redirect_to @book, notice: t('messages.success.update') }
         format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit }
@@ -56,12 +58,16 @@ class BooksController < ApplicationController
   def destroy
     @book.destroy
     respond_to do |format|
-      format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
+      format.html { redirect_to books_url, notice: t('messages.success.destroy') }
       format.json { head :no_content }
     end
   end
 
   private
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 
   # Use callbacks to share common setup or constraints between actions.
   def set_book
