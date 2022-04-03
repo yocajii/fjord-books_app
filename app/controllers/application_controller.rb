9c4cc0bcbcf5d5c2f1c3f353d74cf26dbc489ce8
yocajii <yocajii@stone.sakura.ne.jp>
2022-04-03 20:25:48 +0900

    ログアウト後のリダイレクト先を変更した

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_out_path_for(resource)
+    :books
+  end
+
   protected
 
   def configure_permitted_parameters
