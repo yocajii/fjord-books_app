36fa06fc8d062ab884e6d46036e9a8e9e494b22f
yocajii <yocajii@stone.sakura.ne.jp>
2022-04-06 18:02:56 +0900

    ユーザ情報にアイコン画像の項目を追加
    - ユーザ編集画面にアイコン画像の項目を追加
    - ユーザ詳細画面にアイコン画像の項目を追加
    - 登録可能なファイルを画像ファイルに限定する

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one_attached :avatar
+  validate :validate_avatar, if: :attached?
+
+  private
+
+  def validate_avatar
+    types = %w[image/jpeg image/png image/gif]
+    errors.add(:avatar, 'に登録できるファイルはjpeg/png/gifのいずれかです') unless avatar.content_type.in?(types)
+  end
+
+  def attached?
+    avatar.attached?
+  end
 end
