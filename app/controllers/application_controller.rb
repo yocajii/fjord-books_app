fa930dad8fce29f15c547b6728f8cfd2c1e3e3e9
yocajii <yocajii@stone.sakura.ne.jp>
2022-04-12 16:08:00 +0900

    ログイン後の遷移先をafter_sign_in_path_forで指定するようにした

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(_resource)
+    :books
+  end
+
   def after_sign_out_path_for(_resource)
     :new_user_session
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'books#index'
   resources :books
   resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
