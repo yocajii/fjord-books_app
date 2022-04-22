98b9013fb9d3b719f82d28a9ce8dd72c64e4e49a
yocajii <yocajii@stone.sakura.ne.jp>
2022-04-22 10:21:28 +0900

    リダイレクト先の指定方法を修正した

--- app/controllers/following_relations_controller.rb
+++ app/controllers/following_relations_controller.rb
@@ -4,7 +4,7 @@ class FollowingRelationsController < ApplicationController
   def create
     user = User.find(params[:user_id])
     current_user.follow(user)
-    redirect_to user
+    redirect_to user_path(user)
   end
 
   def destroy
