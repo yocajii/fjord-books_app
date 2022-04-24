7c13a1fb5332fab0757a46638cde2b6d3563484a
yocajii <yocajii@stone.sakura.ne.jp>
2022-04-24 20:01:55 +0900

    Reportのモデルテストを作成した

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,18 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @report = reports(:one)
+    @alice = users(:alice)
+    @bob = users(:bob)
+  end
+
+  test '#editable?' do
+    assert_not @report.editable?(@bob)
+    assert @report.editable?(@alice)
+  end
+
+  test '#created_on' do
+    assert_equal '2022/04/24', I18n.localize(@report.created_on)
+  end
 end
