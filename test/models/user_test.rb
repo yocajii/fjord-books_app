49c825146dd9dc65b0493c07e90bf7856dc3e131
yocajii <yocajii@stone.sakura.ne.jp>
2022-04-24 19:27:15 +0900

    Userモデルテストの残りを作成した

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,12 +3,20 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test '#name_or_email' do
-    user = User.new(email: 'foo@example.com', name: '')
-    assert_equal 'foo@example.com', user.name_or_email
+  test '#following?' do
+    alice = users(:alice)
+    bob = users(:bob)
+    assert_not alice.following?(bob)
+    alice.follow(bob)
+    assert alice.following?(bob)
+  end
 
-    user.name = 'Foo Bar'
-    assert_equal 'Foo Bar', user.name_or_email
+  test '#followed_by?' do
+    alice = users(:alice)
+    bob = users(:bob)
+    assert_not bob.followed_by?(alice)
+    alice.follow(bob)
+    assert bob.followed_by?(alice)
   end
 
   test '#follow' do
@@ -26,5 +34,13 @@ class UserTest < ActiveSupport::TestCase
     assert me.following?(she)
     me.unfollow(she)
     assert_not me.following?(she)
+  end
+
+  test '#name_or_email' do
+    user = User.new(email: 'foo@example.com', name: '')
+    assert_equal 'foo@example.com', user.name_or_email
+
+    user.name = 'Foo Bar'
+    assert_equal 'Foo Bar', user.name_or_email
   end
 end
